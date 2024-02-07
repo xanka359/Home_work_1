9972e70bf5e7446f7dc00cfa0041253531ecf423
Oksana Bondareva <xanka359@yandex.ru>
2024-02-08 00:08:26 +0300

    new shit_2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}, my dear friened!')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}, my dear master!')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
